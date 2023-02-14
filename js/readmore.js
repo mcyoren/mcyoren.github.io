b2cddede4185a3a3765d66777d4e206b238acbab
yoren <mitrankovy@gmail.com>
2023-02-14 21:34:23 +0300

    version 1.3: new publications

--- js/readmore.js
+++ js/readmore.js
@@ -13,3 +13,18 @@ function readmore() {
       moreText.style.display = "inline";
    }
 }
+function readmore_pub() {
+   var dots = document.getElementById("dots_pub");
+   var moreText = document.getElementById("more_pub");
+   var btnText = document.getElementById("btn_pub");
+
+   if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "more publications";
+      moreText.style.display = "none";
+   } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "less publications";
+      moreText.style.display = "inline";
+   }
+}
